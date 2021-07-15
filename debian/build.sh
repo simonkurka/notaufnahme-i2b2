acaefcadea48b3f73518188bf21b86b56818d145
skurka <skurka@ukaachen.de>
2021-07-15 15:13:45 +0200

    remove changelog generation

--- debian/build.sh
+++ debian/build.sh
@@ -17,38 +17,5 @@ cp $DIR/postinst $DBUILD/DEBIAN/
 cp $DIR/prerm $DBUILD/DEBIAN/
 sed -e "/^__I2B2_DROP__/{r $DRESOURCES/database/i2b2_postgres_drop.sql" -e 'd;}' $DIR/postrm > $DBUILD/DEBIAN/postrm
 
-#
-# Changelog
-#
-function getChangelog() {
-	function logentry() {
-		local previous=$1
-		local version=$2
-		local urgency="low"
-		local msg="$(git --no-pager log --format="%B" $previous${previous:+..}$version)"
-		if [[ "$msg" =~ "urgency:"[[:space:]]*"critical" ]]; then urgency="critical"
-		elif [[ "$msg" =~ "urgency:"[[:space:]]*"emergency" ]]; then urgency="emergency"
-		elif [[ "$msg" =~ "urgency:"[[:space:]]*"high" ]]; then urgency="high"
-		elif [[ "$msg" =~ "urgency:"[[:space:]]*"medium" ]]; then urgency="medium"
-		fi
-		echo "$PACKAGE ($version) stable; urgency=$urgency"
-		echo
-		git --no-pager log --format="  * %s" $previous${previous:+..}$version
-		echo
-		git --no-pager log --format=" -- %an <%ae>  %aD" -n 1 $version
-		echo
-	}
-	git tag --sort "-version:refname" | grep "^v\?[0-9]\+\([.-][0-9]\+\)*" | (
-		read version; while read previous; do
-		logentry $previous $version
-		version="$previous"
-	done
-	logentry "" $version
-	)
-}
-
-getChangelog > $DBUILD/DEBIAN/changelog
-editor $DBUILD/DEBIAN/changelog
-
 dpkg-deb --build $DBUILD
 
