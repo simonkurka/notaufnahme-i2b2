2bf1285a95c7391b1ceda55ea32a1f24ffe8a953
skurka <skurka@ukaachen.de>
2021-07-20 14:00:44 +0200

    better handling of docker builds + default namespace

--- docker/build.sh
+++ docker/build.sh
@@ -26,24 +26,27 @@ export DWHIMAGENAMESPACE="$(echo "${PACKAGE}" | awk -F '-' '{print "ghcr.io/"$1"
 . "$(dirname "${DIR}")/build.sh"
 
 mkdir -p "${DBUILD}/wildfly"
-sed -e "s/__VWILDFLY__/${VWILDFLY}/g" wildfly/Dockerfile >"${DBUILD}/wildfly/Dockerfile"
-cp wildfly/entrypoint.sh "${DBUILD}/wildfly/"
+sed -e "s/__VWILDFLY__/${VWILDFLY}/g" "${DIR}/wildfly/Dockerfile" >"${DBUILD}/wildfly/Dockerfile"
+cp "${DIR}/wildfly/entrypoint.sh" "${DBUILD}/wildfly/"
 cp "${DRESOURCES}/standalone.xml.patch" "${DBUILD}/wildfly/"
 wildfly_i2b2 "/wildfly"
 datasource_postinstall "/wildfly/ds"
 
 mkdir -p "${DBUILD}/database"
-cp database/Dockerfile "${DBUILD}/database/"
+cp "${DIR}/database/Dockerfile" "${DBUILD}/database/"
 database_postinstall "/database/sql"
 cat "${DBUILD}/database/sql/i2b2_postgres_init.sql" >"${DBUILD}/database/sql/00_init.sql"
 cat "${DBUILD}/database/sql/i2b2_db.sql" >>"${DBUILD}/database/sql/00_init.sql"
 
 mkdir -p "${DBUILD}/httpd"
-cp httpd/Dockerfile "${DBUILD}/httpd/"
+cp "${DIR}/httpd/Dockerfile" "${DBUILD}/httpd/"
 i2b2_webclient "/httpd/i2b2webclient"
 apache2_proxy "/httpd" "wildfly"
 
 if [ "${FULL}" = "full" ]; then
+	cwd="$(pwd)"
+	cd "${DIR}"
 	docker-compose build
+	cd "${cwd}"
 fi
 
