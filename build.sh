eec2e9c5ac6a67b6180637576ed9bfc5c36a122f
skurka <skurka@ukaachen.de>
2021-07-20 13:24:36 +0200

    common build.sh split into sections

--- build.sh
+++ build.sh
@@ -1,73 +1,106 @@
 #!/bin/bash
 set -e
 
-VI2B2=1.7.12a
-VI2B2_WEBCLIENT=1.7.12a.0002
-VPOSTGRES_JDBC=42.2.8
-VWILDFLY=18.0.0.Final
-
-PACKAGE=$1
-VERSION=$2
-DBUILD=$3
+# Check if variables are empty
+if [ -z "${PACKAGE}" ]; then echo "\$PACKAGE is empty."; exit 1; fi
+if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
+if [ -z "${DBUILD}" ]; then echo "\$DBUILD is empty."; exit 1; fi
 
 # Directory this script is located in + /resources
 DRESOURCES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/resources"
 
-mkdir -p $DBUILD
+set -a
+. "${DRESOURCES}/versions"
+set +a
 
-#
-# Download i2b2 webclient
-#
-mkdir -p $DBUILD/var/www/html
-wget https://github.com/i2b2/i2b2-webclient/archive/v$VI2B2_WEBCLIENT.zip -P $DBUILD
-unzip $DBUILD/v$VI2B2_WEBCLIENT.zip -d $DBUILD/var/www/html
-rm $DBUILD/v$VI2B2_WEBCLIENT.zip
-mv $DBUILD/var/www/html/i2b2-webclient-$VI2B2_WEBCLIENT $DBUILD/var/www/html/webclient
+mkdir -p "${DBUILD}"
 
-sed -i 's|name: \"HarvardDemo\",|name: \"AKTIN\",|' $DBUILD/var/www/html/webclient/i2b2_config_data.js
-sed -i 's|urlCellPM: \"http://services.i2b2.org/i2b2/services/PMService/\",|urlCellPM: \"http://127.0.0.1:9090/i2b2/services/PMService/\",|' $DBUILD/var/www/html/webclient/i2b2_config_data.js
-sed -i 's|loginDefaultUsername : \"demo\"|loginDefaultUsername : \"\"|' $DBUILD/var/www/html/webclient/js-i2b2/i2b2_ui_config.js
-sed -i 's|loginDefaultPassword : \"demouser\"|loginDefaultPassword : \"\"|' $DBUILD/var/www/html/webclient/js-i2b2/i2b2_ui_config.js
+function i2b2_webclient() {
+	DWEBCLIENT="${1}"
 
-#
-# Create apache2 proxy configuration
-#
-mkdir -p $DBUILD/etc/apache2/conf-available
-cp $DRESOURCES/aktin-j2ee-reverse-proxy.conf $DBUILD/etc/apache2/conf-available/aktin-j2ee-reverse-proxy.conf
+	wget "https://github.com/i2b2/i2b2-webclient/archive/v${VI2B2_WEBCLIENT}.zip" -P "${DBUILD}"
+	unzip "${DBUILD}/v${VI2B2_WEBCLIENT}.zip" -d "${DBUILD}"
+	rm "${DBUILD}/v${VI2B2_WEBCLIENT}.zip"
+	mkdir -p "$(dirname "${DBUILD}${DWEBCLIENT}")"
+	mv "${DBUILD}/i2b2-webclient-${VI2B2_WEBCLIENT}" "${DBUILD}${DWEBCLIENT}"
 
-#
-# Download wildfly
-#
-wget https://download.jboss.org/wildfly/$VWILDFLY/wildfly-$VWILDFLY.zip -P $DBUILD
-unzip $DBUILD/wildfly-$VWILDFLY.zip -d $DBUILD/opt
-rm $DBUILD/wildfly-$VWILDFLY.zip
-mv $DBUILD/opt/wildfly-* $DBUILD/opt/wildfly
+	sed -i 's|name: \"HarvardDemo\",|name: \"AKTIN\",|' "${DBUILD}${DWEBCLIENT}/i2b2_config_data.js"
+	sed -i 's|urlCellPM: \"http://services.i2b2.org/i2b2/services/PMService/\",|urlCellPM: \"http://127.0.0.1:9090/i2b2/services/PMService/\",|' "${DBUILD}${DWEBCLIENT}/i2b2_config_data.js"
+	sed -i 's|loginDefaultUsername : \"demo\"|loginDefaultUsername : \"\"|' "${DBUILD}${DWEBCLIENT}/js-i2b2/i2b2_ui_config.js"
+	sed -i 's|loginDefaultPassword : \"demouser\"|loginDefaultPassword : \"\"|' "${DBUILD}${DWEBCLIENT}/js-i2b2/i2b2_ui_config.js"
+}
 
-#
-# Setup wildfly systemd service
-#
-mkdir -p $DBUILD/lib/systemd/system $DBUILD/var/lib/aktin $DBUILD/etc/wildfly
-cp $DBUILD/opt/wildfly/docs/contrib/scripts/systemd/wildfly.service $DBUILD/lib/systemd/system/
-cp $DBUILD/opt/wildfly/docs/contrib/scripts/systemd/wildfly.conf $DBUILD/etc/wildfly/
-cp $DBUILD/opt/wildfly/docs/contrib/scripts/systemd/launch.sh $DBUILD/opt/wildfly/bin/
+function apache2_proxy() {
+	DAPACHE2CONF="${1}"
+	WILDFLYHOST="${2}"
 
-#
-# Customize wildfly
-#
-echo JBOSS_HOME=\"/opt/wildfly\" >> $DBUILD/etc/wildfly/wildfly.conf
-echo JBOSS_OPTS=\"-Djboss.http.port=9090 -Djrmboss.as.management.blocking.timeout=6000\" >> $DBUILD/etc/wildfly/wildfly.conf
+	mkdir -p "${DBUILD}${DAPACHE2CONF}"
+	sed -e "s/__WILDFLYHOST__/${WILDFLYHOST}/g" "${DRESOURCES}/aktin-j2ee-reverse-proxy.conf" >"${DBUILD}${DAPACHE2CONF}/aktin-j2ee-reverse-proxy.conf"
+}
 
-sed -i 's/-Xms64m -Xmx512m/-Xms1024m -Xmx2g/' $DBUILD/opt/wildfly/bin/appclient.conf
-sed -i 's/-Xms64m -Xmx512m/-Xms1014m -Xmx2g/' $DBUILD/opt/wildfly/bin/standalone.conf
+function wildfly_download() {
+	DWILDFLYHOME="$1"
 
-patch -p1 -d $DBUILD/opt/wildfly < $DRESOURCES/standalone.xml.patch
+	wget "https://download.jboss.org/wildfly/${VWILDFLY}/wildfly-${VWILDFLY}.zip" -P "${DBUILD}"
+	unzip "${DBUILD}/wildfly-${VWILDFLY}.zip" -d "${DBUILD}"
+	rm "${DBUILD}/wildfly-${VWILDFLY}.zip"
+	mkdir -p "$(dirname "${DBUILD}${DWILDFLYHOME}")"
+	mv "${DBUILD}/wildfly-${VWILDFLY}" "${DBUILD}${DWILDFLYHOME}"
+}
 
-wget https://www.aktin.org/software/repo/org/i2b2/$VI2B2/i2b2.war -P $DBUILD/opt/wildfly/standalone/deployments/
-wget https://jdbc.postgresql.org/download/postgresql-$VPOSTGRES_JDBC.jar -P $DBUILD/opt/wildfly/standalone/deployments/
+function wildfly_systemd() {
+	DWILDFLYHOME="$1"
+	DWILDFLYCONFIG="$2"
+	DSYSTEMD="$3"
 
-#
-# Post-install resources
-#
-mkdir -p $DBUILD/usr/share/$PACKAGE
-cp -r $DRESOURCES/database $DRESOURCES/datasource $DBUILD/usr/share/$PACKAGE/
+	mkdir -p "${DBUILD}${DWILDFLYCONFIG}" "${DBUILD}${DSYSTEMD}"
+	cp "${DBUILD}${DWILDFLYHOME}/docs/contrib/scripts/systemd/wildfly.service" "${DBUILD}${DSYSTEMD}/"
+	cp "${DBUILD}${DWILDFLYHOME}/docs/contrib/scripts/systemd/wildfly.conf" "${DBUILD}${DWILDFLYCONFIG}/"
+	cp "${DBUILD}${DWILDFLYHOME}/docs/contrib/scripts/systemd/launch.sh" "${DBUILD}${DWILDFLYHOME}/bin/"
+}
+
+function wildfly_config() {
+	DWILDFLYHOME="$1"
+	DWILDFLYCONFIG="$2"
+
+	echo "JBOSS_HOME=\"${DWILDFLYHOME}\"" >> "${DBUILD}${DWILDFLYCONFIG}/wildfly.conf"
+	echo "JBOSS_OPTS=\"-Djboss.http.port=9090 -Djrmboss.as.management.blocking.timeout=6000\"" >> "${DBUILD}${DWILDFLYCONFIG}/wildfly.conf"
+
+	sed -i 's/-Xms64m -Xmx512m/-Xms1024m -Xmx2g/' "${DBUILD}${DWILDFLYHOME}/bin/appclient.conf"
+	sed -i 's/-Xms64m -Xmx512m/-Xms1014m -Xmx2g/' "${DBUILD}${DWILDFLYHOME}/bin/standalone.conf"
+
+	patch -p1 -d "${DBUILD}${DWILDFLYHOME}" < "${DRESOURCES}/standalone.xml.patch"
+}
+
+function wildfly_i2b2() {
+	DWILDFLYDEPLOYMENTS="$1"
+
+	wget "https://www.aktin.org/software/repo/org/i2b2/${VI2B2}/i2b2.war" -P "${DBUILD}${DWILDFLYDEPLOYMENTS}"
+	wget "https://jdbc.postgresql.org/download/postgresql-${VPOSTGRES_JDBC}.jar" -P "${DBUILD}${DWILDFLYDEPLOYMENTS}"
+}
+
+function database_postinstall() {
+	DDBPOSTINSTALL="$1"
+
+	mkdir -p "$(dirname "${DBUILD}${DDBPOSTINSTALL}")"
+	cp -r "${DRESOURCES}/database" "${DBUILD}${DDBPOSTINSTALL}"
+}
+
+function datasource_postinstall() {
+	DDSPOSTINSTALL="$1"
+
+	mkdir -p "$(dirname "${DBUILD}${DDSPOSTINSTALL}")"
+	cp -r "${DRESOURCES}/datasource" "${DBUILD}${DDSPOSTINSTALL}"
+}
+
+function build_linux() {
+	i2b2_webclient "/var/www/html/webclient"
+	apache2_proxy "/etc/apache2/conf-available" "localhost"
+	wildfly_download "/opt/wildfly"
+	wildfly_systemd "/opt/wildfly" "/etc/wildfly" "/lib/systemd/system"
+	wildfly_config "/opt/wildfly" "/etc/wildfly"
+	wildfly_i2b2 "/opt/wildfly/standalone/deployments"
+	database_postinstall "/usr/share/${PACKAGE}/database"
+	datasource_postinstall "/usr/share/${PACKAGE}/datasource"
+}
 
--- debian/build.sh
+++ debian/build.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 set -e
 
-PACKAGE=$1
-VERSION=$2
+# Required parameters
+PACKAGE="${1}"
+VERSION="${2}"
+
+# Check if variables are empty
+if [ -z "${PACKAGE}" ]; then echo "\$PACKAGE is empty."; exit 1; fi
+if [ -z "${VERSION}" ]; then echo "\$VERSION is empty."; exit 1; fi
 
 # Directory this script is located in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-DBUILD="$DIR/build/${PACKAGE}_${VERSION}"
+DBUILD="${DIR}/build/${PACKAGE}_${VERSION}"
+
+# Cleanup
+rm -rf "${DIR}/build"
 
 # Load common linux files
-source $(dirname "$DIR")/build.sh "$PACKAGE" "$VERSION" "$DBUILD"
+. "$(dirname "${DIR}")/build.sh"
+build_linux
 
-mkdir -p $DBUILD/DEBIAN
-sed -e "s/__PACKAGE__/$PACKAGE/g" \
-    -e "s/__VERSION__/$VERSION/g" \
-    $DIR/control > $DBUILD/DEBIAN/control
-sed -e "s/__DWH_SHARED__/$(echo $PACKAGE | awk -F '-' '{print $1"-"$2}')/g" \
-    -e "s/__PACKAGE__/$PACKAGE/g" \
-    $DIR/templates > $DBUILD/DEBIAN/templates
-cp $DIR/config $DBUILD/DEBIAN/
-cp $DIR/postinst $DBUILD/DEBIAN/
-cp $DIR/prerm $DBUILD/DEBIAN/
-sed -e "/^__I2B2_DROP__/{r $DRESOURCES/database/i2b2_postgres_drop.sql" -e 'd;}' $DIR/postrm > $DBUILD/DEBIAN/postrm && chmod 0755 $DBUILD/DEBIAN/postrm
+mkdir -p "${DBUILD}/DEBIAN"
+sed -e "s/__PACKAGE__/${PACKAGE}/g" \
+    -e "s/__VERSION__/${VERSION}/g" \
+    "${DIR}/control" > "${DBUILD}/DEBIAN/control"
+sed -e "s/__DWH_SHARED__/$(echo "${PACKAGE}" | awk -F '-' '{print $1"-"$2}')/g" \
+    -e "s/__PACKAGE__/${PACKAGE}/g" \
+    "${DIR}/templates" > "${DBUILD}/DEBIAN/templates"
+cp "${DIR}/config" "${DBUILD}/DEBIAN/"
+cp "${DIR}/postinst" "${DBUILD}/DEBIAN/"
+cp "${DIR}/prerm" "${DBUILD}/DEBIAN/"
+sed -e "/^__I2B2_DROP__/{r ${DRESOURCES}/database/i2b2_postgres_drop.sql" -e 'd;}' "${DIR}/postrm" > "${DBUILD}/DEBIAN/postrm" && chmod 0755 "${DBUILD}/DEBIAN/postrm"
 
-dpkg-deb --build $DBUILD
+dpkg-deb --build "${DBUILD}"
+rm -rf "${DBUILD}"
 
